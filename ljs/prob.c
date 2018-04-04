0400f53da9f0737e8543bc97df2050c164286362
HongJeSeong <howl78@naver.com>
2018-04-04 12:17:22 +0900

    준성

--- ljs/prob.c
+++ ljs/prob.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define MAX_STACK_SIZE 100
-int check_matching(char data[], int start, int last);
-void inFile(char filename[]);
+int inFile(char data[], int start, int last);
+void checkmatching(char filename[]);
 void result(int state);
 void error(char str[]);
 typedef char Element;
@@ -30,7 +30,7 @@ Element pop()
 {
 	if (is_empty())
 		error("스택 공백 에러");
-	return data[top];
+	return data[top--];
 }
 Element peek()
 {
@@ -39,66 +39,93 @@ Element peek()
 	return data[top];
 }
 
-void inFile(char filename[]) { //파일의 문자열들을 배열에 저장하는 함수
+void checkmatching(char filename[]) //파일의 문자열들을 배열에 저장하는 함수
+{
 	char ch;
-	char expr[1000];
+	char expr[10000];
 	int index = 0;
 	int start = 0;
 
 	FILE *in;
 	in = fopen(filename, "rt");
-	if (in == NULL) {
+	if (in == NULL) 
+	{
 		printf("%s file open error!\n", filename);
 		exit(1);
 	}
-	while ((ch = fgetc(in)) != EOF) {
+	while ((ch = fgetc(in)) != EOF) 
+	{
 		expr[index] = ch;
 		index++;
+		if(ch!='\n')//출력 이쁘게 할라고 넣은거야
 		printf("%c", ch);
-		if (ch == ' ') {//파일의 문자열들을 space(공백)으로 구분하여, 새로운 문자열 등장마다 괄호검사를 호출 
-			result(check_matching(expr, start, index));  //괄호검사 함수 호출  parameter->(배열, 인덱스 시작값, 인덱스 마지막값) 
+		if (ch == '\n')
+		{//파일의 문자열들을 개행(enter)으로 구분하여, 배열에 새로운 문자열이 채워질때 괄호검사를 호출 
+			result(inFile(expr, start, index));  //괄호검사 함수 호출  parameter->(배열, 인덱스 시작값, 인덱스 마지막값) 
 			start = index;		//괄호검사가 끝나면 위치를 바꿔줌
 		}
 		
 	}
 }
 
-int check_matching(char data[],int start, int last) //괄호 검사 함수  안건들였음
+int inFile(char expr[],int start, int last) //괄호 검사 함수
 {
 	int i = start;
 	int  prev;
 	char ch;
-	
+	int check = 0;//껏다 켜지는 스위치로 이해해
 	init_stack();
 
-	for (i; i < last; i++) {
-		ch = data[i];
-		if (ch == '[' || ch == '(' || ch == '{')
-			push(ch);
-		else if (ch == ']' || ch == ')' || ch == '}') {
-			if (is_empty()) 
-				return 2;
-			prev = pop();
-			if ((ch == ']'&&prev != '[') || (ch == ')'&&prev != '(') || (ch == '}'&&prev != '{')) {
-				return 3;
+	for (i; i < last; i++)
+	{
+		ch = expr[i];
+
+		if (ch == 39) {	//따옴표를 의미하는 아스키코드
+			if (check == 0) 
+			{
+				check = 1;
+			}
+			else if (check == 1) 
+			{
+				check = 0;
+			}
+		}
+		if (check != 1) //flase 일때만 검사한다. 즉, 따옴표가 한번도 나오지 않는다면 0일테고, 한번나오면 1일테고.. 두번 나오면 다시 0인 상태로 돌아가겄지 ~
+		{
+			if (ch == '[' || ch == '(' || ch == '{')
+				push(ch);
+			else if (ch == ']' || ch == ')' || ch == '}')
+			{
+				if (is_empty())
+					return 2;
+				prev = pop();
+				if ((ch == ']'&&prev != '[') || (ch == ')'&&prev != '(') || (ch == '}'&&prev != '{'))
+				{
+					return 3;
+				}
 			}
 		}
 	} 
-	if(is_empty()) 
+	if(is_empty()==0) 
 		return 1;
 
 	return 0;
 }
 
-void result(int state) { //정상, 오류 결과 출력 함수
-	if (state == 0) {
+ 
+void result(int state)//정상, 오류 결과 출력 함수
+{
+	if (state == 0)
+	{
 		printf(": 정상\n");
 	}
-	else {
+	else 
+	{
 		printf(": 규칙 %d위반\n", state);
 	}
 }
+
 void main()
 {
-	inFile("lab3.txt");
+	checkmatching("소스.c");
 }
